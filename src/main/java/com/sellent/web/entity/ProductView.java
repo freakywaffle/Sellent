9f8e0770274880c2e103f37e344c8311f5193f4b
freakywaffle <joewkd@daum.net>
2019-02-14 10:26:22 +0900

    hh

--- src/main/java/com/sellent/web/entity/ProductView.java
+++ src/main/java/com/sellent/web/entity/ProductView.java
@@ -4,23 +4,31 @@ import java.util.Date;
 
 public class ProductView extends Product{
 	private int reviewCnt;
-
+	private String nickname;
+	
 	public ProductView() {
 	}
-	
+
 	public ProductView(int no, String title, String parentCategory, String subCategory, String writerId,
 			String simpleContent, String detailContent, int price, int sellCheck, int duration, int editCnt,
-			double avgStarPoint, Date regdate, int approval, int reviewCnt) {
+			double avgStarPoint, Date regdate, int approval, int reviewCnt, String nickname) {
 		super(no, title, parentCategory, subCategory, writerId, simpleContent, detailContent, price, sellCheck,
 				duration, editCnt, avgStarPoint, regdate, approval);
 		this.reviewCnt = reviewCnt;
+		this.nickname = nickname;
 	}
 
 	public ProductView(String title, String parentCategory, String subCategory, String writerId, String simpleContent,
-			String detailContent, int price, int sellCheck, int duration, int editCnt, int reviewCnt) {
+			String detailContent, int price, int sellCheck, int duration, int editCnt, int reviewCnt, String nickname) {
 		super(title, parentCategory, subCategory, writerId, simpleContent, detailContent, price, sellCheck, duration,
 				editCnt);
 		this.reviewCnt = reviewCnt;
+		this.nickname = nickname;
+	}
+
+	public ProductView(int reviewCnt, String nickname) {
+		this.reviewCnt = reviewCnt;
+		this.nickname = nickname;
 	}
 
 	public int getReviewCnt() {
@@ -30,6 +38,16 @@ public class ProductView extends Product{
 	public void setReviewCnt(int reviewCnt) {
 		this.reviewCnt = reviewCnt;
 	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	
 	
 	
 }
