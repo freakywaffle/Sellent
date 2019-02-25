ecc4a50d768db732dc5a95f1ed9094d5f351f8df
asg0828 <44486817+asg0828@users.noreply.github.com>
2019-02-25 10:23:37 +0900

    admin

--- src/main/java/com/sellent/web/controller/admin/ConfigController.java
+++ src/main/java/com/sellent/web/controller/admin/ConfigController.java
@@ -1,7 +1,9 @@
 package com.sellent.web.controller.admin;
 
-import java.io.File; 
+import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -66,47 +68,57 @@ public class ConfigController {
 	}
 	
 	@PostMapping("bannerInsert")
-	@ResponseBody
 	public String bannerInsert(
-			//Banner banner,
-			String title, String endDate, String content,
 			MultipartHttpServletRequest multipartHttpServletRequest
-			) throws IllegalStateException, IOException {
+			) throws IllegalStateException, IOException, ParseException {
 		
-//		int order = adminBannerDao.getBannerCnt()+1;
-//		
-//		banner.setTitle(title);
-//		banner.setEnd_date(endDate);
-//		banner.setContent(content);
-//		
+		SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
+		
+		int order = adminBannerDao.getBannerCnt()+1;
+		
+		Banner banner = new Banner();
+		
+		banner.setTitle(multipartHttpServletRequest.getParameter("title"));
+		banner.setEnd_date(multipartHttpServletRequest.getParameter("endDate"));
+		banner.setContent(multipartHttpServletRequest.getParameter("content"));
+		banner.setOrder(order);
+		
 		// 파일 경로
 		String filePath = "F:\\sellent\\admin\\";
 		
 		// 파라미터명이 files인 파일을 가져온다 -> List형태로 담는다
 		List<MultipartFile> files = multipartHttpServletRequest.getFiles("image");
 		
-		System.out.println(files.size());
+		//System.out.println(files.get(0).getOriginalFilename());
+		banner.setImage(files.get(0).getOriginalFilename());
 		
-//		// 파일객체에 경로를 담는다 (디렉토리, 파일 형식 둘다 가능)
-//		File file = new File(filePath);
-//		
-//		// 객체가 가지고 있는 경로가 없으면 디렉토리 생성
-//		if(file.exists() == false) {
-//			file.mkdir();
-//		}
-//		
-//		for(int i =0; i<files.size(); i++) {
-//		
-//			file = new File(filePath+files.get(i).getOriginalFilename());
-//			files.get(i).transferTo(file);	// 객체에 저장된 경로로  파일을 전송한다
-//		}
+		int result = adminBannerDao.insert(banner);
 		
-		return "ok";
+		// 파일객체에 경로를 담는다 (디렉토리, 파일 형식 둘다 가능)
+		File file = new File(filePath);
+		
+		// 객체가 가지고 있는 경로가 없으면 디렉토리 생성
+		if(file.exists() == false) {
+			file.mkdir();
+		}
+		
+		for(int i =0; i<files.size(); i++) {
+		
+			file = new File(filePath+files.get(i).getOriginalFilename());
+			files.get(i).transferTo(file);	// 객체에 저장된 경로로  파일을 전송한다
+		}
+		
+		return "redirect:banner";
 	}
 	
-	@PostMapping("bannerDelete")
-	public String bannerDelete() {
-		
+	@PostMapping("bannerRemove")
+	@ResponseBody
+	public String boardRemove(@RequestParam(name = "arr") ArrayList<Integer> arr) {
+
+		for (int i = 0; i < arr.size(); i++) {
+
+			int result = adminBannerDao.delete(arr.get(i));
+		}
 		return "ok";
 	}
 	
